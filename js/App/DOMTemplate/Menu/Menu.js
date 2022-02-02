230c801ffb0a73d90f57aabe301a3fefbf531ee2
Frank-Paredes <frankparedesalpi@gmail.com>
2022-02-02 08:49:55 -0500

    DOMTemplate

--- js/App/DOMTemplate/Menu/Menu.js
+++ js/App/DOMTemplate/Menu/Menu.js
@@ -8,5 +8,10 @@ class DT_Menu extends DOMTemplate {
         (option, idx) => new DT_MenuOption(option, idx == 0 ? true : false)
       ),
     });
+    $("body").on("click", "#menu > .option", (event) => {
+      this.templateStruct.childs.forEach((item) =>
+        item.template.removeClass("selected")
+      );
+    });
   }
 }
